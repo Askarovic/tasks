a6b32026895142bed9bcd3f704072601c1ee108a
Askar <askarashimov@gmail.com>
2022-02-21 10:24:37 +0600

    fix: correct tags

--- 18-02/1/script.js
+++ 18-02/1/script.js
@@ -1,24 +1,22 @@
 /*let num = 10;
-num=++ num;
+++ num;
  console.log(num);
 
 let num =10;
-num =-- num;
+-- num;
 console.log(num);
 
 let num = 10;
-num =++num;
+num++;
 console.log(num);*/
 
 /*
 let name = 'Askar!';
-let name1 = 'Привет ';
-alert(name1 + name);
+
+alert(`Привет + name`);
 
 let age = 18;
-let me = 'Мне ';
-let me1 = ' лет!';
-alert(me + age + me1);*/
+alert(`Мне + age + лет`);*/
 
 /*
 
@@ -28,7 +26,7 @@ let c = a - b;
 console.log(c);
 let d = 7;
 console.log(d+c);
-let result = 14;
+let result = c + d;
 alert(result);
 */
 /*
@@ -47,7 +45,3 @@ let num = 47;
        let num = 47;
        num /= 15;
        alert(num);*/
-fdsdsfasdfsda
-
-
-
