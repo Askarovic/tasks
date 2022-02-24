56c04d3c3766dfe37ee797b93ddfa7996996ad32
Askar <askarashimov@gmail.com>
2022-02-24 13:55:07 +0600

    finish

---
+++ 24-02/script.js
@@ -0,0 +1,18 @@
+/* let arr = ['a', 'b', 'c', 'd']
+console.log(arr[0], " " + "+", arr[1]) */
+
+
+ /* let arr = [10, 15, 9, 10]
+ let result = 10*15 + 9*10; 
+ alert(result) */
+ /* */
+let arr = ['a', 'b', 'c'];
+arr.push (1, 2, 3);
+console.log(arr);
+
+/* let arr = [1,2,3,4,5,6,7,8,9,10]
+for(let i=0; i<=9; i++){
+    console.log(arr[i]*2);
+} */
+
+
